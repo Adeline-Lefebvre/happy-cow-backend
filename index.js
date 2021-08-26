e058083d2c360a6df54c18ce69ba67624eba7ab8
Adeline Lefebvre <adelinelefebvre@MacBook-Pro-de-Adeline.local>
2021-08-26 18:38:47 +0200

    message

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.post("login", async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.fields.email });
 
